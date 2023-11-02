a4ee6774fcee7fde2c8eba28ec43e1a6e221842b
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-11-02 12:43:43 -0400

    Add price info

--- AirbnbClone/Views/AirbnbDetailView.swift
+++ AirbnbClone/Views/AirbnbDetailView.swift
@@ -16,31 +16,50 @@ struct AirbnbDetailView: View {
                 ScrollView(.vertical) {
                     // Picture
                     AsyncImage(url: URL(string: model.xl_picture_url ?? ""))
-                        .aspectRatio(contentMode: .fill)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: proxy.frame(in: .global).width, height: proxy.frame(in: .global).width)
                         .clipped()
                     
                     // Info
                     Text(model.name ?? "")
                         .bold()
+                        .padding()
+                    
+                    if let price = model.price {
+                        Text("Nightly Rate: \(price.formatted(.currency(code: "USD")))")
+                    }
                     
                     Text("Description: \(model.description ?? "")")
+                        .padding()
                     Text("Summary: \(model.summary ?? "")")
+                        .padding()
                     Text("House Rules: \(model.house_rules ?? "")")
+                        .padding()
                     Text("Space: \(model.space ?? "")")
+                        .padding()
                     
                     //Host Info
+                    Text("About your Host")
+                        .font(.title2)
+                        .bold()
+                    
                     HStack {
-                        AsyncImage(url: URL(string: model.host_picture_url))
+                        AsyncImage(url: URL(string: model.host_thumbnail_url))
                             .frame(width: 75, height: 75)
                             .aspectRatio(contentMode: .fill)
                             .clipShape(Circle())
                         
                         Text(model.host_name)
                             .bold()
+                            
                     }
+                    .padding()
+                    
+                    Text("Hosting since: "+model.host_since)
                 }
-                
+                .frame(maxWidth: proxy.frame(in: .global).width)
             }
         }
+        .navigationTitle(model.name ?? "Listing")
     }
 }
